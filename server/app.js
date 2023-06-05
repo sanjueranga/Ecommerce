fe7c8ab80c37d8c2379bd9cecc28dbcf562bfdeb
sanjueranga <sanjueranga16@gmail.com>
2023-06-05 21:18:00 +0530

    login and register jwt , cookies

--- server/app.js
+++ server/app.js
@@ -9,9 +9,15 @@ app.use(express.json());
 
 //Import all routes
 const products = require('./routes/product');
+const auth = require('./routes/auth');
+// const payment = require('./routes/payment');
+// const order = require('./routes/order');
 
-app.use('/',products);
 
+app.use('/', products);
+app.use('/api/v1', auth);
+// app.use('/api/v1', payment)
+// app.use('/api/v1', order)
 app.use(errorMiddleware);
 
 
