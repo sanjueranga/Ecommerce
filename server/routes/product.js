a352c163138f20d02f7d1a721e8ace0a834f3242
sanjueranga <sanjueranga16@gmail.com>
2023-06-05 21:50:20 +0530

    done user

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,19 +1,35 @@
 const express = require('express')
 const router = express.Router();
 
-const {getProducts , newProduct, getSingleProduct,updateProduct,deleteProduct,getAdminProducts} = require('../controllers/productController');
-const { route } = require('../app');
+
+const {
+    getProducts,
+    getAdminProducts,
+    newProduct,
+    getSingleProduct,
+    updateProduct,
+    deleteProduct,
+   
+ 
+
+} = require('../controllers/productController')
+
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 
 router.route('/products').get(getProducts);
 router.route('/admin/products').get(getAdminProducts);
 router.route('/product/:id').get(getSingleProduct);
 
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+
+router.route('/admin/product/:id')
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 
-router.route('/admin/products/:id').delete(deleteProduct);
+// router.route('/review').put(isAuthenticatedUser, createProductReview)
+// router.route('/reviews').get(isAuthenticatedUser, getProductReviews)
+// router.route('/reviews').delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
-
-
-
